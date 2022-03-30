8d4898d6e8977ca8e26fd33ebdc7331ba7e92300
Luis Solís <37953927+solisgb@users.noreply.github.com>
2022-03-30 21:39:48 +0200

    Add files via upload

--- gw-levels/import_data.sql
+++ gw-levels/import_data.sql
@@ -63,7 +63,7 @@ with CSV header delimiter ','
 encoding 'UTF-8'
 ;
 
--- Ahora toca arreglar 
+-- Revisión de columnas numéricas prof y pnp
 select *
 from tmp.chspz t 
 where t.pnp !~ '^[0-9\.]+$'
@@ -80,8 +80,68 @@ where pnp !~ '^[0-9\.]+$'
 returning *
 ;
 
+-- Arreglo de la columna fmed , el problema es que las fechas están formadas de 2 maneras diferentes
+select concat(split_part(fmed, '/', 3), '-', split_part(fmed, '/', 1), '-', split_part(fmed, '/', 2), ' 00:00:00')::timestamp fmed1 
+from tmp.chspz t
+where fmed ~ '/' --mm/dd/yyyy
+;
+
+update tmp.chspz t 
+set strtmp = concat(split_part(fmed, '/', 3), '-', split_part(fmed, '/', 1), '-', split_part(fmed, '/', 2), ' 00:00:00')
+where fmed ~ '/'
+returning strtmp::timestamp 
+;
+
+select concat(split_part(fmed, '-', 3), '-', split_part(fmed, '-', 2), '-', trim(split_part(fmed, '-', 1)), ' 00:00:00')::timestamp fmed1
+from tmp.chspz t
+where fmed ~ '-' --dd/mm/yyyy
+;
+
+update tmp.chspz t 
+set strtmp = concat(split_part(fmed, '-', 3), '-', split_part(fmed, '-', 2), '-', trim(split_part(fmed, '-', 1)), ' 00:00:00')
+where fmed ~ '-'
+returning strtmp::timestamp 
+;
+
+select t.strtmp:: timestamptz fmed1 
+from tmp.chspz t
+;
+
+-- table intermedia
+create table tmp.ipa2 as 
+select *
+from ipas.ipa2 
+with no data
+;
+
+alter table tmp.ipa2
+add primary key (cod, fecha)
+;
+
+-- primero relleno la tabla ipa2 en el esquema tmp a partir de los datos facilitados en hoja de cálculo 
+insert into tmp.ipa2 (cod, fecha, situacion, pnp, instalado, tuboguia, tr, proyecto, codigosonda, medidor, pnp_original)
+select t.id , t.strtmp::timestamp , 'e', t.pnp::real , 't', 't', 0, 'chs_red_pz', NULL, NULL, t.pnp::real
+from tmp.chspz t
+on conflict do nothing
+;
+
+-- ahora tengo que sustituir id oficial por id tipo igme
 
 
+-- primero veo si están todos
+select 
+from tmp.ipa2 i 
+	join 
+
+
+
+
+--insert into tmp.ipa2 (cod, fecha, situacion, pnp, instalado, tuboguia, tr, proyecto, codigosonda, medidor, pnp_original) 
+select t.cod, t2.fecha, t2.situacion, t2.pnp, t2.instalacionsn, t2.tuboguiasn, t2.tr, t2.proyecto, t2.codigosonda, t2.observ, t2.medidor, t2.pnp_original 
+from tmp.ipa2 t
+on conflict on constraint ipa2_pkey
+do update set pnp_original = t.excluded.pnp_original
+;
 
 
 
